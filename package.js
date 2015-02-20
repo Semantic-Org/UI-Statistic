a923c2429ffce8348c444d78947b39ef50915bb4
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:16:23 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-statistic',
-  summary : 'Semantic UI - Statistic (official): Single component release of statistic',
-  version : '1.9.2',
+  summary : 'Semantic UI - Statistic: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Statistic.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'statistic.css'
+  ], 'client');
 });
