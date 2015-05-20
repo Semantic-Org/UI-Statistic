eeb39d7d1a50ff89909781c04d0a42a51f9e56a6
Pusher Robot <jack@semantic-ui.com>
2015-05-20 11:58:50 -0400

    Updated component to version 1.12.3

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-statistic',
   summary : 'Semantic UI - Statistic: Single component release',
-  version : '1.12.2',
+  version : '1.12.3',
   git     : 'git://github.com/Semantic-Org/UI-Statistic.git',
 });
 
