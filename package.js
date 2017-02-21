3ab67223fd0b5ee704893e66d1d238260b3604c0
Jack Lukic <jack@semantic-ui.com>
2017-02-21 01:14:04 -0500

    Updated component to version 2.2.8

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-statistic',
   summary : 'Semantic UI - Statistic: Single component release',
-  version : '2.2.7',
+  version : '2.2.8',
   git     : 'git://github.com/Semantic-Org/UI-Statistic.git',
 });
 
